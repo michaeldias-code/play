07397963367f049ff803381b9db94751c212ce5b
michaeldias-code <michaelrpdias@gmail.com>
2025-12-12 16:15:18 -0300

    Add files via upload

---
+++ chess/config.js
@@ -0,0 +1,15 @@
+// config.js - desatualizado sem EnPassant e AI_Easy, AI_Hard, AI_Medium
+export const Modules = {
+  Board: 'Board',
+  Piece: 'Piece',
+  MoveValidator: 'MoveValidator',
+  GameController: 'GameController',
+  AI: 'AI',
+  View: 'View'
+};
+
+export const Classes = {
+  Board: 'Board',
+  Piece: 'Piece',
+  AI: 'AI'
+};
